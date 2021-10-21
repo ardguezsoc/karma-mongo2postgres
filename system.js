f34e6e9b22add3ddf3f2b9832983d64ae7e0b473
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2021-10-21 19:42:08 +0200

    chore: set indentation to 2 spaces

--- system.js
+++ system.js
@@ -2,4 +2,4 @@ const System = require('systemic');
 const { join } = require('path');
 
 module.exports = () => new System({ name: 'karma-mongo2postgres' })
-	.bootstrap(join(__dirname, 'components'));
+  .bootstrap(join(__dirname, 'components'));
