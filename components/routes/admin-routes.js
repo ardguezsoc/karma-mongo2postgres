ff47f546414a2cf9ccd27eefa33015cb794aa32c
Àlex Serra <bounteous17@protonmail.com>
2021-11-05 18:04:07 +0100

    testing input data endpoint

--- components/routes/admin-routes.js
+++ components/routes/admin-routes.js
@@ -23,8 +23,15 @@ module.exports = () => {
     */
     app.get('/__/manifest', (req, res) => res.json(manifest));
 
-    app.get('/v1/run', async (/* req, res */) => {
+    /**
+     * This endpoint imports new data from the bot MongoDB to the stats PostgreSQL DB
+     * @route GET /var/run
+     * @group Admin - Everything about admin routes
+     * @returns 200 - Sucessful response
+    */
+    app.get('/v1/run', async (request, response) => {
       await controller.run();
+      response.status(200).send();
     });
 
     return Promise.resolve();
